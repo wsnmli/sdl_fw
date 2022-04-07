4809b9da2f2a32697528776395fc2e159e0baba6
wsnmli <05wsnmli@gmail.com>
2022-04-07 18:44:13 +0100

    added some stuff

--- examples/Wang_tiles/main.cpp
+++ examples/Wang_tiles/main.cpp
@@ -1,8 +1,29 @@
 //g++ main.cpp -lSDL2 -lSDL2_ttf  && ./a.out 
 #include "../../sdl_fw.h"
 
+#define WIDTH (700)
+#define HEIGHT (800)
+#define NUMBER (16)   // number of wang tiles
+
 using namespace std;
 
+class wangTile {};
+
+
 int main() {
-    cout << "H" << endl;
+    Framework fw("Wang Tiles", WIDTH, HEIGHT);
+    SDL_Event event;
+    fw.set_offset(0, 0);
+    fw.set_background_colour(0, 0, 255);
+
+    // wang tiles
+
+    //game loop
+    while(!(event.type == SDL_QUIT)) {      SDL_Delay(20);
+        SDL_PollEvent(&event);
+        fw.blank();
+        fw.draw_line(Vector(0,0), Vector(WIDTH,HEIGHT));
+        // reneder Wang tiles
+        fw.render();
+    }
 }
