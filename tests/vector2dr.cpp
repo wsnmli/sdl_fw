c4032daa0249ef2108361caea69d0c48f2dad78e
wsnmli <05wsnmli@gmail.com>
2022-04-11 14:32:46 +0100

    added simple tests for vector2d.h

---
+++ tests/vector2dr.cpp
@@ -0,0 +1,11 @@
+//  g++ vector2dr.cpp && ./a.out 
+#include <cassert>
+
+#include "../vector2d.h"
+using namespace std;
+
+int main() {
+    Vector z(0., 0.), a(1., 1.), b(2., 2.) ;
+    assert(a*0 == z); assert(0*a == z);
+    assert(a*2 == b);
+}
