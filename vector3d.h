60d0dbb8df9de4e6b2644b7e9a451bc83f08bc7a
wsnmli <05wsnmli@gmail.com>
2022-04-13 18:20:05 +0100

    bulked up vector3d

--- vector3d.h
+++ vector3d.h
@@ -9,12 +9,51 @@ class Vector3 { public:
     float x, y, z;
 
     // constructors 
-    Vector(float i, float j, float k) {
+    Vector3(float i, float j, float k) {
         x = i; y = j; z = k;
     }
-    Vector() {
+    Vector3() {
         x = 0; y = 0; z = 0;
     }
+    Vector3 operator* (float c) {
+        Vector3 tmp;
+        tmp.x = c * x;
+        tmp.y = c * y;
+        tmp.z = c * z;
+        return tmp; 
+    }
+    Vector3 operator+ (Vector3 a) {
+    Vector3 tmp;
+    tmp.x = x + a.x;
+    tmp.y = y + a.y;
+    tmp.z = z + a.z;
+    return tmp;
+    }
+
+    Vector3 operator- (Vector3 a) {
+        Vector3 tmp;
+        tmp.x = x - a.x;
+        tmp.y = y - a.y;
+        tmp.z = z - a.z;
+        return tmp;
+    }
+    void operator-= (Vector3 z) {
+        *this = *this - z;
+    }
+
+void operator+= (Vector3 z) {
+    *this = *this + z;
+}
 };
+
+int operator==(Vector3 a, Vector3 b) {
+    return (a.x == b.x && a.y == b.y && a.z == b.z);
+}
+
+Vector3 operator*(float a, Vector3 b) {
+    return b*a;
+}
+
+
 
 #endif
