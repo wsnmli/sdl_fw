b961f395dac8e6d8b2d7d2540a62341eff024626
Matthew J Lindsay <05wsnmli@gmail.com>
2022-04-13 21:01:41 +0100

    bulked it out a bit

--- examples/Graph_Drawer/node.cpp
+++ examples/Graph_Drawer/node.cpp
@@ -1,8 +1,13 @@
 //  g++ node.cpp -lSDL2 -lSDL2_ttf  && ./a.out
 #include "../../sdl_fw.h"
+#include "../../vector2d.h"
 
-class Node { public:
-
+class Node :public Vector{ 
+    public:
+    int highlighted;    // highlights when mouse is hovering over
+    Node(int i, int j) {
+        x = i; y = z;
+    }
 };
 
 int main() {
