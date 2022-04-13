a9fbe338ae1328d42ecc04591bc686ff807cedbf
wsnmli <05wsnmli@gmail.com>
2022-04-13 19:03:03 +0100

    bulked out idea

--- examples/3D_Drawer/cube.cpp
+++ examples/3D_Drawer/cube.cpp
@@ -1,10 +1,32 @@
 //  g++ cube.cpp -lSDL2 -lSDL2_ttf  && ./a.out
 #include "../../sdl_fw.h"
+#include "../../vector3d.h"
 
-class Cube {
+#include <vector>
+using namespace std;
 
+class Vertex :public Vector3 { public:
+    Vertex(float i, float j, float k) {
+        x = i; y = j; z = k;
+    }
+    void draw() {}
+};
+
+class Cube { private:
+    vector<Vertex> vertices;
+public:
+    Cube() {
+        for (int i=0; i<16; i++) {
+            vertices.push_back(Vertex(0,0,0));
+        }
+    }
+};
+
+class Scene {
+    Cube cube;
+    Vector3 origin();
 };
 
 int main() {
-
+    Scene scene;
 }
