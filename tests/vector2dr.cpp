2ccae86576a869c6e590246865364389b4a839bb
wsnmli <05wsnmli@gmail.com>
2022-04-13 17:57:23 +0100

    sectioned tests

--- tests/vector2dr.cpp
+++ tests/vector2dr.cpp
@@ -5,7 +5,10 @@
 using namespace std;
 
 int main() {
+    // +, -, *, /
     Vector z(0., 0.), a(1., 1.), b(2., 2.) ;
     assert(a*0 == z); assert(0*a == z);
     assert(a*2 == b); assert(a+a == b);
+    assert(b-a==a);
+    // +=, -=, *=, /=
 }
