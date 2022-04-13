14eccdbe302216479643883407a522be1c0fec26
wsnmli <05wsnmli@gmail.com>
2022-04-13 19:19:02 +0100

    bulked out idea a little

--- examples/3D_Drawer/cube.cpp
+++ examples/3D_Drawer/cube.cpp
@@ -12,9 +12,8 @@ class Vertex :public Vector3 { public:
     void draw() {}
 };
 
-class Cube { private:
+class Cube { public:
     vector<Vertex> vertices;
-public:
     Cube() {
         for (int i=0; i<16; i++) {
             vertices.push_back(Vertex(0,0,0));
@@ -23,10 +22,36 @@ public:
 };
 
 class Scene {
+    Framework* fw;
+
     Cube cube;
     Vector3 origin();
+public:
+    Scene(Framework& a) {
+        fw = &a;
+        fw->set_background_colour(0,0,255);
+    }
+    
+    int getinput() {
+        // return 1 if player wants to quit
+        
+        return 0;
+    }
+    void draw() {
+        fw->blank();
+        // draw each vertex in cube
+        for (int i = 0; i < cube.vertices.size(); i++) {}
+
+        fw->render();
+    }
 };
 
-int main() {
-    Scene scene;
+int main() { 
+    Framework fw("3D Cube", WIDTH, HEIGHT);
+    Scene scene(fw);
+    int done = 0;
+    while(!done) {
+        done = scene.getinput();
+        scene.draw();
+    }
 }
