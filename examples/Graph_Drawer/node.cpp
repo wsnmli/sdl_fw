9e2882f5cb35846fabb07c7a441c2eed6ceb68a3
Matthew J Lindsay <05wsnmli@gmail.com>
2022-04-17 19:32:38 +0100

    bulked out node.cpp

--- examples/Graph_Drawer/node.cpp
+++ examples/Graph_Drawer/node.cpp
@@ -3,13 +3,26 @@
 #include "../../vector2d.h"
 
 class Node :public Vector{ 
+    Framework* fw;
     public:
     int highlighted;    // highlights when mouse is hovering over
-    Node(int i, int j) {
-        x = i; y = z;
+    Node(Framework& a, int i, int j) {
+        x = i; y = j;
+        fw = &a;
+    }
+    void draw() {
+        if(highlighted) { //draw red circle
+            fw->draw_circle(Vector(x,y),255,255,0,0);
+        }
     }
 };
 
 int main() {
+    Framework fw("Node_Drawer", WIDTH, HEIGHT);
+    while(!(fw.event.type == SDL_QUIT)) {
+        SDL_PollEvent(&fw.event);
+        fw.blank();
 
+        fw.render();
+    }
 }
