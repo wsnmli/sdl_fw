ae14ac459f57eb83ede6dec2ad91cbcf336d51ce
Matthew J Lindsay <05wsnmli@gmail.com>
2022-04-26 17:03:53 +0100

    added two simple tests.

--- tests/vector2dr.cpp
+++ tests/vector2dr.cpp
@@ -9,6 +9,7 @@ int main() {
     Vector z(0., 0.), a(1., 1.), b(2., 2.) ;
     assert(a*0 == z); assert(0*a == z);
     assert(a*2 == b); assert(a+a == b);
+    assert(a+b==b+a);
     assert(b-a==a);
     // +=, -=, *=, /=
 
@@ -16,4 +17,5 @@ int main() {
     assert(z==a);
     z-=a;
     assert(z==Vector(0,0));
+    assert(b/2 == a);
 }
