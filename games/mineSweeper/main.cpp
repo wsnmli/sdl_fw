ccaee7f18e84f7254e18f45046f90b109e8988e1
Matthew J Lindsay <05wsnmli@gmail.com>
2022-04-29 21:53:49 +0100

    added file to gitignore

--- games/mineSweeper/main.cpp
+++ games/mineSweeper/main.cpp
@@ -1,3 +1,27 @@
+//  g++ main.cpp -lSDL2 -lSDL2_ttf  && ./a.out 
+#include "../../sdl_fw.h"
+
+class Square {
+    Framework* a;
+public:
+    int x, y;   //postion
+    int width = 20;
+    int height = width;
+
+    int value;  // value on square
+    int minep; // is mine?
+
+    Square(Framework& fw) {
+        a = &fw;
+        x = 40; y = 110;
+    }
+
+    void show() {
+        a->draw_black_rectangle(x,y,width,height);
+        
+    }
+};
+
 int main() {
     
 }
