e838335aceab5d794c2c718cc01ff8afac75c610
matt <05wsnmli@gmail.com>
2022-06-18 14:14:19 +0100

    little_progress

--- fractual/cantorFractual/main.cpp
+++ fractual/cantorFractual/main.cpp
@@ -2,17 +2,23 @@
 //  g++ -g3 main.cpp -lSDL2 -lSDL2_ttf  && gdb ./a.out 
 #include "../../sdl_fw.h"
 
-void cantor(Framework& fw, int iteration) {
-    fw.draw_circle(Vector(WIDTH/2, HEIGHT/2), 50);
+void cantor(Framework& fw, int iteration, int x, int y, int width) {
+    int height = 50;
+    
+    if (iteration == 0) {
+        fw.draw_rectangle(x, y, width, height);
+    }
 }
 
 int main() {
     Framework fw("Cantor Fractal", WIDTH, HEIGHT);
 
+    int start_height = 50/2;
+
     while(fw.event.type != SDL_QUIT) {
         SDL_PollEvent(&fw.event);
         fw.blank();
-        cantor(fw, 0);
+        cantor(fw, 0, WIDTH/2, start_height, WIDTH - 10);
         fw.render();
     }
 }
